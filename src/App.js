5d9a1aa0e6264e39c6fdcd531580ea7dae62157b
Huy Nguyen <thesamurai3011@gmail.com>
2023-08-19 07:26:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -134,10 +134,6 @@ function App() {
     )
   }
 
-  console.log({ checks, amounts, total, cart })
-  console.log(localStorage)
-
-
   return (
     <div className="App_mainContent">
       <div className="App_card">
